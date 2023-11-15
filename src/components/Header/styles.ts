93d352f8d586178e68966be854e090e1c4fd55b8
biel.lil <78098857+biellil@users.noreply.github.com>
2023-11-15 14:15:26 -0300

    Update styles.ts

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,7 +3,6 @@ import styled, { css } from 'styled-components'
 export const HeaderContainer = styled.header`
   width: 100%;
   height: 6.5rem;
-  background: ${({ theme }) => theme.colors['base-background']};
   display: flex;
   align-items: center;
   justify-content: center;
